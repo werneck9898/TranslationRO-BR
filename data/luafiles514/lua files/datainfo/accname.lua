7b8720ee87dcf343aeb7d2d2a62a00795babbc3d
werneck <werneck.mateus@gmail.com>
2020-09-15 15:10:44 -0300

    System and Data Folders

---
+++ data/luafiles514/lua files/datainfo/accname.lua
@@ -0,0 +1,14 @@
+AccNameTable = {
+
+	[ACCESSORY_IDs.ACCESSORY_Low_Pony_BU] = "_Low_Pony_BU",
+	[ACCESSORY_IDs.ACCESSORY_Low_Pony_RD] = "_Low_Pony_RD",
+	[ACCESSORY_IDs.ACCESSORY_Low_Pony_YL] = "_Low_Pony_YL",
+	[ACCESSORY_IDs.ACCESSORY_Low_Pony_GN] = "_Low_Pony_GN",
+	[ACCESSORY_IDs.ACCESSORY_Low_Pony_BL] = "_Low_Pony_BL",
+	[ACCESSORY_IDs.ACCESSORY_Low_Pony_WH] = "_Low_Pony_WH",
+	[ACCESSORY_IDs.ACCESSORY_Low_Pony_OM] = "_Low_Pony_OM",
+	[ACCESSORY_IDs.ACCESSORY_Low_Pony_PP] = "_Low_Pony_PP",
+	[ACCESSORY_IDs.ACCESSORY_Vajra] = "_πŸ¡Í∂Û",
+	[ACCESSORY_IDs.ACCESSORY_C_Spider_Temp_TW] = "_Spider_Temp_TW",
+
+}
